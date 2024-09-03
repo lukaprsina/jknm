fdf4f12247cb55b069c72926ecf2127cabcb079d
Luka Pršina <prsinaluka@gmail.com>
2024-09-03 12:58:40 +0200

    added all packages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,70 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "jamarski-klub-novo-mesto.s3.eu-central-1.amazonaws.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "jknm.s3.eu-central-1.amazonaws.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.jknm.si",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.google.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "jknm-turborepo.vercel.app",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+
+  experimental: {
+    reactCompiler: true,
+    serverActions: {
+      // TODO
+      bodySizeLimit: "100mb",
+    },
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+};
 
 export default config;
