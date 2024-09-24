513e5d659e10c2a1e2e3c1dc2e47d50183113263
Luka Pršina <prsinaluka@gmail.com>
2024-09-24 19:17:08 +0200

    working on zgodovina

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,25 @@
 import type { MDXComponents } from "mdx/types";
 import { TableOfContents } from "~/components/static/toc-scroll";
+import type { ImageProps } from "next/image";
 import Image from "next/image";
+import React from "react";
+
+function ImageWithCaption({
+  caption,
+  ...props
+}: ImageProps & { caption: React.ReactNode }) {
+  return (
+    <figure>
+      <Image {...props} />
+      <figcaption>{caption}</figcaption>
+    </figure>
+  );
+}
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     TableOfContents,
-    Image,
+    Image: ImageWithCaption,
     ...components,
   };
 }
--- scripts/image.py
+++ scripts/image.py
@@ -2,8 +2,8 @@ import os
 from glob import glob
 from pathlib import Path
 import re
-import uuid
 from sanitize_filename import sanitize
+
 
 def convert_title_to_url(dangerous_url: str) -> str:
     # Sanitize the filename
@@ -11,39 +11,92 @@ def convert_title_to_url(dangerous_url: str) -> str:
 
     # Perform replacements
     replaced = sanitized.lower()
-    replaced = re.sub(r'–', '-', replaced)
-    replaced = re.sub(r'č', 'c', replaced)
-    replaced = re.sub(r'š', 's', replaced)
-    replaced = re.sub(r'ž', 'z', replaced)
-    replaced = re.sub(r'[^a-zA-Z0-9\-_\s]', '', replaced)  # Remove invalid characters
-    replaced = re.sub(r'_+', '_', replaced)  # Replace sequences of underscores
-    replaced = replaced.strip('-_')  # Remove leading/trailing hyphens and underscores
+    replaced = re.sub(r"–", "-", replaced)
+    replaced = re.sub(r"č", "c", replaced)
+    replaced = re.sub(r"š", "s", replaced)
+    replaced = re.sub(r"ž", "z", replaced)
+    replaced = re.sub(r"[^a-zA-Z0-9\-_\s]", "", replaced)  # Remove invalid characters
+    replaced = re.sub(r"_+", "_", replaced)  # Replace sequences of underscores
+    replaced = replaced.strip("-_")  # Remove leading/trailing hyphens and underscores
     replaced = replaced.strip()
 
     # Split, trim, and filter out empty elements
-    replaced_split = [s.strip() for s in re.split(r'[\s+]', replaced) if s.strip()]
+    replaced_split = [s.strip() for s in re.split(r"[\s+]", replaced) if s.strip()]
 
-    return '_'.join(replaced_split)
+    return "_".join(replaced_split)
+
 
 base_path = Path.cwd() / "src/app/(static)"
 
-def iterate_page(directory):
-    result = list(Path(directory).rglob('*.jpg'))
-    print("page", directory)
+sorted_dirs = [
+    "1962 Ustanovitev",
+    "1978 Ponovni zagon",
+    "1982–1987 Topliško obdobje",
+    "1988–1995 Gabrsko obdobje",
+    "1996–2001 Tečajniško obdobje",
+    "2002–2008 Mirnopeško obdobje",
+    "2008–2016 Čagankarsko obdobje",
+    "2017–2022 Obdobje presežkov",
+]
 
-    for entry in result:        
-        relative = Path(entry).relative_to(directory)
+
+def iterate_page_subdir(directory: Path):
+    imports = []
+
+    files = os.listdir(directory)
+    for entry in files:
+        absolute = directory / entry
+        relative = absolute.relative_to(directory.parent)
+        stem = absolute.stem[9:]
+        name = convert_title_to_url(stem)
+
+        slug = name
+        index = 1
+        while any(slug == existing_name for existing_name, _, _ in imports):
+            slug = f"{name}_{index}"
+            index += 1
+
         fixed_relative = str(relative).replace("\\", "/")
-        name = convert_title_to_url(entry.stem)[9:]
-        print(f"import {name} from \"./{fixed_relative}\"")
+        imports.append((slug, fixed_relative, stem))
 
-def main():    
-    for entry in os.listdir(base_path):
+    return imports
+
+
+def iterate_page(directory: Path):
+    print(f"page {directory}: {sorted_dirs}")
+
+    for test in sorted_dirs:
+        test = directory / test
+        if os.path.isdir(test):
+            print("subdir", test)
+            imports_set = iterate_page_subdir(test)
+
+            for slug, relative, stem in imports_set:
+                print(f'import {slug} from "./{relative}"')
+
+            print()
+
+            for slug, relative, stem in imports_set:
+                print(f'<Image src={{{slug}}} alt="{stem}" caption="" />')
+
+            input()
+            print()
+            print()
+            print()
+        else:
+            print(f"missing {test}")
+    print(f"done with {directory}")
+
+
+def main():
+    iterate_page(Path(base_path) / "zgodovina")
+    """ for entry in os.listdir(base_path):
         absolute = os.path.join(base_path, entry)
         if os.path.isfile(absolute):
             continue
         if os.path.isdir(absolute):
-            iterate_page(absolute)
+            iterate_page(absolute) """
+
 
 if __name__ == "__main__":
     main()
