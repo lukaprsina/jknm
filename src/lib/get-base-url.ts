eb97fc4b20efbdd3329ac406ec45bbe387104410
Luka Pršina <prsinaluka@gmail.com>
2024-10-14 15:37:25 +0200

    NEXTAUTH_URL

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -1,5 +1,7 @@
+import { env } from "~/env";
+
 export function get_base_url(force_domain?: boolean): string {
   if (!force_domain && typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (env.NEXTAUTH_URL) return `https://${env.NEXTAUTH_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
