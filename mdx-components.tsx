7345f2f8e9a9a9951eceff90b91309147e7e3ac4
Luka Pršina <prsinaluka@gmail.com>
2024-10-22 12:58:12 +0200

    added link to mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,15 @@
 import type { MDXComponents } from "mdx/types";
 import { ImageWithCaption } from "~/components/image-with-caption";
 import { TableOfContents } from "~/components/static/toc-scroll";
+import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     strong: (props) => <b {...props} />,
-    // TODO: add links
+    a: ({ href, ...props }) => {
+      if (typeof href === "undefined") throw new Error("href is undefined");
+      return <Link href={href} {...props} />;
+    },
     TableOfContents,
     Image: ImageWithCaption,
     ...components,
