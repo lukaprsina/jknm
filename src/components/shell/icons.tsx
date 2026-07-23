941b0908096877645c4fcae6607108071f9e3a23
Luka Pršina <prsinaluka@gmail.com>
2026-07-23 14:18:48 +0200

    sponsors, footer

--- src/components/shell/icons.tsx
+++ src/components/shell/icons.tsx
@@ -46,7 +46,7 @@ export function YoutubeIcon() {
 		<Tooltip>
 			<TooltipTrigger asChild>
 				<Link
-					href="https://www.youtube.com/@MrKlemi"
+					href="https://www.youtube.com/@JamarskiKlubNovomesto"
 					target="_blank"
 					className={cn(
 						buttonVariants({ variant: "ghost", size: "icon" }),
@@ -75,7 +75,7 @@ export function InstagramIcon() {
 		<Tooltip>
 			<TooltipTrigger asChild>
 				<Link
-					href="https://www.youtube.com/@MrKlemi"
+					href="https://www.instagram.com/jamarskiklubnm/"
 					target="_blank"
 					className={cn(
 						buttonVariants({ variant: "ghost", size: "icon" }),
@@ -94,35 +94,6 @@ export function InstagramIcon() {
 				</Link>
 			</TooltipTrigger>
 			<TooltipContent>Instagram</TooltipContent>
-		</Tooltip>
-	);
-}
-
-// simpleicons.org
-export function TwitterIcon() {
-	return (
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<Link
-					href="https://www.youtube.com/@MrKlemi"
-					target="_blank"
-					className={cn(
-						buttonVariants({ variant: "ghost", size: "icon" }),
-						"h-9 w-9 rounded-full p-0 text-center",
-						"transition-colors hover:text-black",
-					)}
-				>
-					<svg
-						role="img"
-						viewBox="0 0 24 24"
-						className="h-6 w-6 fill-current"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-					</svg>
-				</Link>
-			</TooltipTrigger>
-			<TooltipContent>X (Twitter)</TooltipContent>
 		</Tooltip>
 	);
 }
